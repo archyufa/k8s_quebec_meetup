20022d071b538c7488d3743383415aaf154ab7d5
Ayrat Khayretdinov <ayrat.khayretdinov@gmail.com>
2017-04-27 18:00:11 -0400

    update go file

--- demo2/build.sh
+++ demo2/build.sh
@@ -1,4 +1,4 @@
 # Build a statically linked Linux executable to be used in the Docker container
 # This assumes a properly configured Go installation
 
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+CGO_ENABLED=0 GOOS=linux /usr/local/go/bin/go build -a -installsuffix cgo -o main .
